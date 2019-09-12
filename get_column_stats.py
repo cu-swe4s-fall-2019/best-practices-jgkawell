23796a158a8688a682ec31632485291df76ce3f4
Jack Kawell <jgkawell@outlook.com>
2019-09-12 10:03:47 -0600

    Fixed style issues and added argparse to get_column_stats.py

--- get_column_stats.py
+++ get_column_stats.py
@@ -1,15 +1,27 @@
-import sys, math
+import sys
+import math
+import argparse
 
-file_name = sys.argv[1]
-col_num = int(sys.argv[2])
+parser = argparse.ArgumentParser(
+    description='Get the stats from a given column in a given file.')
+parser.add_argument(
+    '--file_name',
+    type=str,
+    help='The file to read')
+parser.add_argument(
+    '--col_num',
+    type=int,
+    help='The column to calculate stats for (starts at 0)')
 
-f = open(file_name, 'r')
+args = parser.parse_args()
+
+f = open(args.file_name, 'r')
 
 V = []
 
 for l in f:
     A = [int(x) for x in l.split()]
-    V.append(A[col_num])
+    V.append(A[args.col_num])
 
 mean = sum(V)/len(V)
 
